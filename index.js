3ddd89a5722eef310ffe901178d4ddd024f5b1ff
AmirMasoud Beheshti <am.beheshti@deltaban.local>
2022-06-06 22:05:55 +0430

    feat: create template and create component for react

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi')
+#!/usr/bin/env node
+const fs = require("fs");
+const yargs = require("yargs/yargs")(process.argv.slice(2)).argv;
+const {source} = require('./template/react');
+const Handlebars = require("handlebars");
+
+const template= Handlebars.compile(source);
+const [component, componentName] = yargs._;
+if (component === "react") {
+  fs.appendFile(`${componentName}.jsx`, template({name:componentName}), (err)=> {
+    throw err
+  });
+} else {
+  console.error("Something Wrong !");
+}
---
+++ template/react.js
@@ -0,0 +1,10 @@
+ const source = `import React from 'react'
+const {{name}} = () => {
+  return (
+    <div>{{name}}</div>
+  )
+}
+
+export default {{name}}`
+
+module.exports = {source}
